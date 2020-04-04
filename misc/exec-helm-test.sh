d3b56d7d10e1c552372228b216603a0636373161
Ryu Sato <ryu@weseek.co.jp>
2020-04-04 23:56:51 +0900

    debug

--- misc/exec-helm-test.sh
+++ misc/exec-helm-test.sh
@@ -129,6 +129,7 @@ main() {
     # (see. https://github.com/helm/helm/issues/7857)
     helm dependency update ${CHART}
     helm install --wait --timeout ${timeout} ${HELM_DEBUG_OPT} ${CHART} ${CHART}
+    helm status ${CHART}
     kubectl get pod -o wide -w
     helm test ${HELM_DEBUG_OPT} ${CHART}
     helm uninstall ${HELM_DEBUG_OPT} ${CHART}
